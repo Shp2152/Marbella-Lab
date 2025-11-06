738dc53b9563e356f7fae284502a172ebf5d5956
Susie Park <shp2152@columbia.edu>
2025-11-06 17:35:44 -0500

    Delete arbin_res_load.py

--- arbin_res_load.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import electrochem as echem
-
-
-def parse_arbinres(file_path: str, save_path: str) -> pd.DataFrame:
-    echem.parseArbin(file_path, save_path)
-    df = pd.read_csv(save_path)
-    return df
